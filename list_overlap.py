46b57429566dd1a89c00153827663ac760d6f6cd
Jacob Tran <jacobntran@gmail.com>
2022-01-23 18:51:52 -0800

    solved extras

--- list_overlap.py
+++ list_overlap.py
@@ -1,18 +1,26 @@
+import random 
+
 # take two lists and return list that has common elements between the lists (no duplicates)
 
 # need a loop to iterate
 # intialize empty list to hold the common elements
 # if element in list 1 and is in list 2 but NOT in result list, add to result list
 
-list1 = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-list2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+list1 = [random.randint(1,20) for i in range(10)]
+list2 = [random.randint(1,20) for i in range(10)]
+
+list1.sort()
+list2.sort()
+
+print(list1)
+print(list2)
 
 def common_integers(l1, l2):
-    result = []
+    result = list(set([el for el in l1 if el in l2]))
 
-    for i in l1:
-        if i in l2 and i not in result:
-            result.append(i)
+    # for i in l1:
+    #     if i in l2 and i not in result:
+    #         result.append(i)
     
     return result
 
