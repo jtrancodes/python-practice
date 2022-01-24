4d9fd986e0f9cf0f4f670aa1030e078c1f50b19e
Jacob Tran <jacobntran@gmail.com>
2022-01-23 18:12:38 -0800

    solved extras

--- less_than_10.py
+++ less_than_10.py
@@ -1,7 +1,15 @@
+# EXTRA
+num = int(input('Input a number: '))
+
 # create function that takes in list and prints all elements less than 5
 def less_than_5(l):
-    for el in l:
-        if el < 5: 
-            print(el)
+    # result = []
+    # for el in l:
+    #     if el < 5: 
+    #         result.append(el)
+    
+    # return result
 
-less_than_5([1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89])
+    return [el for el in l if el < num]
+
+print(less_than_5([1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]))
