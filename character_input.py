5be9767ec1896bde8765ff4ae279674476cfc934
Jacob Tran <jacobntran@gmail.com>
2022-01-23 16:30:10 -0800

    solved extras

--- character_input.py
+++ character_input.py
@@ -3,8 +3,8 @@ from datetime import date
 # create input that asks for users name and age
 name = input('What is your name? ')
 age = int(input('How old are you? '))
-
+frequency = int(input('How many times would you like us to repeat the response? '))
 # print out message that tells the user the year that they will turn 100
 years_until_100 = date.today().year - age + 100
 
-print(f'{name} will turn 100 in the year {years_until_100}.') 
+print(f'{name} will turn 100 in the year {years_until_100}. \n' * frequency) 
