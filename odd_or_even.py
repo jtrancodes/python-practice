ea9d2641893d8ad6ee43e131143a22241b6f31d0
Jacob Tran <jacobntran@gmail.com>
2022-01-23 16:47:10 -0800

    solved extras

--- odd_or_even.py
+++ odd_or_even.py
@@ -2,7 +2,17 @@
 num = int(input('Input a number: '))
 
 # print appropriate message depending on if the num is even or odd
-if num % 2 == 0:
-    print(f'{num} is an even number!')
-else:
-    print(f'{num} is an odd number!')
+# if num % 4 == 0:
+#     print(f'{num} is a multiple of 4!')
+# elif num % 2 == 0:
+#     print(f'{num} is an even number!')
+# else:
+#     print(f'{num} is an odd number!')
+
+# EXTRA
+check = int(input('Provide another number: '))
+
+if num % check == 0:
+    print(f'{check} divides evenly into {num}.')
+else: 
+    print(f'{check} does not divide evenly into {num}.')
