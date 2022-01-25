591a2b9ea578dfc0e5f4ef3fb797dd7fe3f79acb
Jacob Tran <jtran@way2b1.com>
2022-01-24 17:26:43 -0800

    solved extras

--- guessing_game1.py
+++ guessing_game1.py
@@ -1,14 +1,24 @@
 import random 
+guesses = 0
 
-winning_num = random.randint(1,9)
+while True:
+    winning_num = random.randint(1,9)
+    user_answer = input('Choose a number from 1 to 9 or Type exit to leave game. ')
 
-user_num = int(input('Choose a number from 1 to 9: '))
+    if user_answer == 'exit':
+        break
 
-print(winning_num)
+    user_num = int(user_answer)
 
-if user_num > winning_num:
-    print(f'Your guess {user_num} is too high.')
-elif user_num < winning_num:
-    print(f'Your guess {user_num} is too low.')
-else:
-    print(f'Your guess {user_num} is correct!')
+    if user_num > winning_num:
+        print(f'Your guess {user_num} is too high.')
+    elif user_num < winning_num:
+        print(f'Your guess {user_num} is too low.')
+    else:
+        print(f'Your guess {user_num} is correct!')
+        guesses += 1
+        break
+
+    guesses += 1
+
+print(f'You guessed {guesses} times.')
