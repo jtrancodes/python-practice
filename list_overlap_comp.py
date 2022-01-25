b76d240ce6639c1f1ca0541308ffc582121302e0
Jacob Tran <jtran@way2b1.com>
2022-01-24 17:53:00 -0800

    solved extras

--- list_overlap_comp.py
+++ list_overlap_comp.py
@@ -1,4 +1,15 @@
+import random
+
 def common_elements(l1,l2):
     return list(set([el for el in l1 if el in l2]))
 
-print(common_elements([1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]))
+l1 = random.sample(range(1,20),8)
+l2 = random.sample(range(1,20),15)
+l1.sort()
+l2.sort()
+print(l1)
+print(l2)
+
+# print(common_elements([1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]))
+
+print(common_elements(l1,l2))
